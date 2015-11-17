e015e0d8444940c4002f2d772590c953d3df6b62
Jhonatan Garcia <tomaz_shulo@hotmail.com>
2015-11-17 09:06:39 -0800

    se hicieron las ventanas , pendiente crear tablas

---
+++ proyecto/buss.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto
+{
+    public class buss
+    {
+        public int codigobus {get;set;}
+        public String tipo_bus {get; set;}
+        public String marca_bus {get;set;}
+        public String color_bus { get; set; }
+
+    }
+}
---
+++ proyecto/nuevaventana.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace proyecto
+{
+    /// <summary>
+    /// Interaction logic for nuevaventana.xaml
+    /// </summary>
+    public partial class nuevaventana : Window
+    {
+        public nuevaventana()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+           
+        }
+    }
+}
